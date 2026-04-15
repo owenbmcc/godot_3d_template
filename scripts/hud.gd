272d87783e251432fd539d1c9ff5f4c32a2a5543
owenbmcc <oroberts@bmcc.cuny.edu>
2026-04-15 16:58:29 -0400

    hud updates

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,14 +1,29 @@
+"""
+for hud (head's up display)
+console for messages and counter for items, lives etc
+
+basic node setup (will need additional ui nodes)
+
+• Control (hud) # hud.gd
+	• Label (console)
+	• Label (counter)
+
+connect signals from portal, items collected etc.
+release mouse: set hud > Control > Mouse > Filter > Pass
+"""
 extends Control
 
+@export var console : Label
+@export var counter : Label
+
 func _ready():
-	$CenterContainer2/Console.text = ""
+	console.text = ""
 
 func _on_collectible_item_collected():
-	$CenterContainer/AppleCount.text = str(global.apple_count)
+	counter.text = str(global.apple_count)
 
 func _on_pickup_area_update_console(message):
-	$CenterContainer2/Console.text = message
+	console.text = message
 
 func _on_portal_update_console(message):
-
-	$CenterContainer2/Console.text = message
+	console.text = message
